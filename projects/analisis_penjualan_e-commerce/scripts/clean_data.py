168043591ad8ce5143749568e2011af987484b48
Adib Raihan Ashidiq <119989067+Boekanadip@users.noreply.github.com>
2025-04-09 02:06:49 +0700

    Update clean_data.py

--- projects/analisis_penjualan_e-commerce/scripts/clean_data.py
+++ projects/analisis_penjualan_e-commerce/scripts/clean_data.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+def load_and_clean_data(filepath):
+    df = pd.read_csv(filepath)
+    
+    # Drop missing values
+    df.dropna(inplace=True)
+
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    # Convert datetime
+    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+
+    # Create TotalRevenue column
+    df['TotalRevenue'] = df['Quantity'] * df['UnitPrice']
+
+    return df
+
+if __name__ == "__main__":
+    df_clean = load_and_clean_data("data/ecommerce_sales.csv")
+    df_clean.to_csv("data/ecommerce_sales_cleaned.csv", index=False)
+    print("Data berhasil dibersihkan dan disimpan.")
